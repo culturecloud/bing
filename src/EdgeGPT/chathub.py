9cc03f031fc076e37c07f94735446f4f79d84b2a
iafuad.bd@gmail.com <iafuad.bd@gmail.com>
2023-12-23 23:27:22 +0600

    update chathub.py

--- src/EdgeGPT/chathub.py
+++ src/EdgeGPT/chathub.py
@@ -217,14 +217,12 @@ class ChatHub:
                             f"{response['item']['result']['value']}: {response['item']['result']['message']}",
                         )
                     if draw:
-                        print(resp_txt)
-                        # cache = response["item"]["messages"][1]["adaptiveCards"][0][
-                        #     "body"
-                        # ][0]["text"]
-                        # response["item"]["messages"][1]["adaptiveCards"][0]["body"][0][
-                        #     "text"
-                        # ] = (cache + resp_txt)
-                        print(response["item"]["messages"])
+                        cache = response["item"]["messages"][2]["adaptiveCards"][0][
+                            "body"
+                        ][0]["text"]
+                        response["item"]["messages"][2]["adaptiveCards"][0]["body"][0][
+                            "text"
+                        ] = (cache + resp_txt)
                     if (
                         response["item"]["messages"][-1]["contentOrigin"] == "Apology"
                         and resp_txt
