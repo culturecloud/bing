34d331020cfbdc60ec7c5951f3c11a049fff7c31
iafuad.bd@gmail.com <iafuad.bd@gmail.com>
2023-12-23 23:51:46 +0600

    update chathub.py

--- src/EdgeGPT/chathub.py
+++ src/EdgeGPT/chathub.py
@@ -166,6 +166,7 @@ class ChatHub:
                             try:
                                 async with ImageGenAsync(
                                     all_cookies=self.cookies,
+                                    quiet=True
                                 ) as image_generator:
                                     images = await image_generator.get_images(
                                         response["arguments"][0]["messages"][0]["text"],
@@ -216,13 +217,6 @@ class ChatHub:
                         raise Exception(
                             f"{response['item']['result']['value']}: {response['item']['result']['message']}",
                         )
-                    if draw:
-                        cache = response["item"]["messages"][2]["adaptiveCards"][0][
-                            "body"
-                        ][0]["text"]
-                        response["item"]["messages"][2]["adaptiveCards"][0]["body"][0][
-                            "text"
-                        ] = (cache + resp_txt)
                     if (
                         response["item"]["messages"][-1]["contentOrigin"] == "Apology"
                         and resp_txt
